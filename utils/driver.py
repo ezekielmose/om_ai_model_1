de8b9587ab3a39813ab81a3d6f2ebd232d424d49
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2026-04-30 08:37:28 +0300

    Update driver.py

--- utils/driver.py
+++ utils/driver.py
@@ -1,20 +1,2 @@
-import undetected_chromedriver as uc
-
-
-def get_driver(headless=False):
-    options = uc.ChromeOptions()
-
-    if headless:
-        options.add_argument("--headless=new")
-
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    driver = uc.Chrome(
-    version_main=147,   # ✅ FIX
-    options=options
-)
-
-    return driver
+def get_driver(*args, **kwargs):
+    return None
